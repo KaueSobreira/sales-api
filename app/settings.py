f11983cac961da942944d943cac60044f2310cfc
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-25 20:39:44 -0300

    feat: add drf-spectacular for API documentation and update URL routing

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'drf_spectacular',
+
     'rest_framework',
 ]
 
@@ -121,3 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Sales-Api',
+    'DESCRIPTION': 'API RESTful construída com Django e Django REST Framework, explorando conceitos como autenticação, versionamento e boas práticas de arquitetura.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_SETTINGS': {
+        'defaultModelsExpandDepth': -1,
+        'docExpansion': 'none',
+    },
+}
