e5a2d50f935fbbb696426ca3d84acff2bd5e185c
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-25 21:30:29 -0300

    feat: enhance 'products' serializers and views to support nested brand and category data

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from .models import Products
+from brands.serializers import BrandSerializer
+from categories.serializers import CategorieSerializer
 
+
+class ProductSerializerBFF(serializers.ModelSerializer):
+    brands = BrandSerializer()
+    categorie = CategorieSerializer()
+
+    class Meta:
+        model = Products
+        fields = ['id', 'name', 'brands', 'stock', 'price', 'categorie', 'is_stock']
 
 class ProductSerializer(serializers.ModelSerializer):
-
 
     class Meta:
         model = Products
--- products/views.py
+++ products/views.py
@@ -1,12 +1,21 @@
 from rest_framework import generics
 from .models import Products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductSerializerBFF
 
 
 class ProductCreateListView(generics.ListCreateAPIView):
     queryset = Products.objects.all()
-    serializer_class = ProductSerializer
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ProductSerializerBFF
+        return ProductSerializer
+
 
 class ProductUpdateDeleteListView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
-    serializer_class = ProductSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ProductSerializerBFF
+        return ProductSerializer
